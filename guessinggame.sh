3a8a72fc74c65086109c47df632b13e892a006da
yashr1209 <yash.raj@cs.christuniversity.in>
2020-07-19 15:15:22 +0530

    modified gesssinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,20 +2,21 @@ function guessNumberOfFiles(){
 numFile=$(ls -l | grep "^-" | wc -l)
 while true;
 do
-	echo "Please enter a number: "
+echo "Please enter a number: "
 read guess
 if [ $guess -lt $numFile ]
 then
-	echo "Your guess is less than the real number, try a slightly higher number."
+echo "Your guess is less than the real number, try a slightly higher number."
 elif [ $guess -gt $numFile ]
 then
-	echo "Your guess is higher than the real number, try a lower number."
+echo "Your guess is higher than the real number, try a lower number."
 else
-	echo "Bravo! Your guess is Corect!"
+echo "Bravo! Your guess is Corect!"
 break;
 fi
 done
 
 }
-echo "Guess the number of files in the curent directoy"
+
+echo "Guess the number of files in the current directory"
 guessNumberOfFiles
